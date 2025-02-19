355cab05b58edbc174bb3cbf3a15699614e75c8f
alex <haiiryy@gmail.com>
2025-02-19 00:30:25 -0300

    fix production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import solid from '@astrojs/solid-js';
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable Solid to support Solid JSX components.
   integrations: [solid()],
 
   vite: {
